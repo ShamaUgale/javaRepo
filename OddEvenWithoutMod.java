928fa1698ee66e79c4d011b80954b4585d4119a5
Shama Ugale <shama.ugale@gmail.com>
2021-02-13 00:14:23 +0530

    odd even without mod operator
    java code

---
+++ OddEvenWithoutMod.java
@@ -0,0 +1,20 @@
+static boolean isEven(int n)
+{
+return ((n / 2) * 2 == n);
+}
+
+
+public static void main (String[] args)
+{
+int n = 57;
+
+if(isEven(n))
+
+System.out.println(“Even”);
+
+else
+
+System.out.println(“Odd”);
+
+}
+}
