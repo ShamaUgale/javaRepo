c5b0e0b1069e9a3f57129ffe39d31b4370501c81
Shama Ugale <shama.ugale@gmail.com>
2021-04-22 00:03:47 +0530

    Two Number Sum Problem
    Given an array of integers, return the indices of the two numbers whose sum is equal to a given target.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    Example:
    
    Given nums = [2, 7, 11, 15], target = 9.
    
    The output should be [0, 1]. 
    Because nums[0] + nums[1] = 2 + 7 = 9.

---
+++ IndexOfTwoNumSum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class TwoSum {
+
+    // Time complexity: O(n*log(n))
+    private static int[] findTwoSum_Sorting(int[] nums, int target) {
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right) {
+            if(nums[left] + nums[right] == target) {
+                return new int[] {nums[left], nums[right]};
+            } else if (nums[left] + nums[right] < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[] {};
+    }
+}
