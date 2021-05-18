b6947b6a41d6610731029e1863ebf9db1b60b1eb
Shama Ugale <shama.ugale@gmail.com>
2021-05-18 22:58:45 +0530

    chess board
    return if the square is black or white 
    
    input - a1
    output - black
    
    i/p - b2
    o/p - white

---
+++ ChessBoradSquareColor.java
@@ -0,0 +1,33 @@
+public final class Chess {
+    public boolean isBlack(String field) {
+        char[] digs = field.toCharArray();
+        return (isOddColumn(digs[0]) && isOddRow(digs[1])) || (isEvenColumn(digs[0]) && isEvenRow(digs[1]));
+    }
+
+    public boolean isWhite(String field) {
+        return !isBlack(field);
+    }
+
+    private boolean isOddColumn(char col) {
+        return col == 'A' || col == 'C' || col == 'E' || col == 'G';
+    }
+
+    private boolean isEvenColumn(char col) {
+        return col == 'B' || col == 'D' || col == 'F' || col == 'H';
+    }
+
+    private boolean isOddRow(char row) {
+        return row == '1' || row == '3' || row == '5' || row == '7';
+    }
+
+    private boolean isEvenRow(char row) {
+        return row == '2' || row == '4' || row == '6' || row == '8';
+    }
+
+    public static void main(String[] args) {
+        Chess app = new Chess();
+        System.out.println(app.isBlack("A1"));
+        System.out.println(app.isBlack("B2"));
+        System.out.println(app.isBlack("C3"));
+    }
+}
